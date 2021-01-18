0537d5d34d5c6a5a02dd7ddfbe11f55da6ce067f
edenave <76529301+edenave@users.noreply.github.com>
2021-01-18 17:53:07 +0200

    Q6 FINISHED

---
+++ q6.js
@@ -0,0 +1,32 @@
+const david = 50;
+const vinoth = 50;
+const divya = 50;
+const ishitha = 70;
+const thomas = 70;
+
+let avgGrade = (david + vinoth + divya + ishitha + thomas) / 5;
+
+if (avgGrade < 60) {
+
+console.log('F')
+}
+else if (avgGrade < 70) {
+
+    console.log('D')
+
+}
+else if (avgGrade < 80) {
+
+    console.log('C')
+
+}
+else if (avgGrade < 90) {
+
+    console.log('B')
+
+}
+else if (avgGrade < 100) {
+
+    console.log('A')
+
+}    
