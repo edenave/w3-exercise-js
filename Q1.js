3e5526597c1e9b24ea91f091fa21608eceeca4c8
edenave <76529301+edenave@users.noreply.github.com>
2021-01-18 16:26:59 +0200

    Q1 finished

---
+++ Q1.js
@@ -0,0 +1,10 @@
+let number1 = Number( prompt("Insert first number"));
+let number2 = Number( prompt("Insert second number"));
+
+
+    if (number1 > number2){
+    alert(number1);
+
+} else {
+    alert(number2)
+}
