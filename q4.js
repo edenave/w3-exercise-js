ca11d76a09e4a7385b76b8e718822641c4c67540
edenave <76529301+edenave@users.noreply.github.com>
2021-01-18 17:30:14 +0200

    q4 finished

---
+++ q4.js
@@ -0,0 +1,29 @@
+const a = 1;
+const b = -13;
+const c = 4;
+const d = -7;
+const e = -100;
+
+if(a > b && a > c && a > d && a > e){
+
+    alert(a);
+}
+
+else if (b > a && b > c && b > d && b > e){
+
+    alert(b);
+}
+
+else if (c > a && c > b && c > d && c > e){
+    
+    alert(c);
+}
+
+else if (d > a && d > c && d > b && d > e){
+    
+    alert(d);
+}
+else if (e > a && e > c && e > d && e > b){
+    
+    alert(e);
+}
