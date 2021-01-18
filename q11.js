6a8f518d3579f778dda892c81c6ee91a562c3177
edenave <76529301+edenave@users.noreply.github.com>
2021-01-18 19:47:46 +0200

    q11 finished

---
+++ q11.js
@@ -0,0 +1,10 @@
+let number1 = Number(prompt("enter the first number"));
+let number2 = Number(prompt("enter the second number"));
+let gcd = number1 < number2 ? number1 : number2;
+let divisor = 1;
+for (let i=1; i<gcd; i++){
+    if(number1 % i === 0 && number2 % i === 0 ){
+        devisor =  i;
+    }
+}
+alert(devisor);
